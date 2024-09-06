450e2527889cd3aa2a08b6291efae4d62b747883
Leonardo <leo-nino70@outlook.com>
2024-09-05 22:09:04 -0300

    Adicionando tabela categoria

--- Aula03.sql
+++ Aula03.sql
@@ -1,16 +1,23 @@
-CREATE DATABASE Biblioteca;
-USE Biblioteca;
+CREATE DATABASE Biblioteca9;
+USE Biblioteca9;
 
 CREATE TABLE Autor(
 	ID_Autor INT AUTO_INCREMENT PRIMARY KEY,
     nomeAutor VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE Categoria(
+	ID_Categoria INT AUTO_INCREMENT PRIMARY KEY,
+	nomeCategoria VARCHAR(100)
+);
+
 CREATE TABLE Livros(
 	ID_Livro INT AUTO_INCREMENT PRIMARY KEY,
     tituloLivro VARCHAR(200) NOT NULL,
     IDautor INT,
-    FOREIGN KEY (IDautor) REFERENCES Autor(ID_Autor)
+    IDcategoria INT,
+    FOREIGN KEY (IDautor) REFERENCES Autor(ID_Autor),
+    FOREIGN KEY (IDcategoria) REFERENCES Categoria(ID_Categoria)
 );
 
 INSERT INTO Autor (nomeAutor) VALUES ('George R.R Martin');
@@ -20,6 +27,9 @@ INSERT INTO Livros(tituloLivro, IDautor) VALUES ('A muralha de gelo', 1);
 INSERT INTO Livros(tituloLivro, IDautor) VALUES ('A dança dos dragões', 1);
 INSERT INTO Livros(tituloLivro, IDautor) VALUES ('O Murder of Rager Ackroyd', 2);
 
-SELECT Livros.tituloLivro, Autor.nomeAutor AS Autor
+SELECT Livros.tituloLivro, Autor.nomeAutor, Categoria.nomeCategoria AS Autor
 FROM Livros
-JOIN Autor ON Livros.IDautor = Autor.ID_Autor;
+JOIN Autor ON Livros.IDautor = Autor.ID_Autor
+JOIN Categoria ON Livros.IDcategoria = Categoria.ID_Categoria;
+
+
